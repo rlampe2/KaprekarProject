6679efbc8ace9ea4806ab589be7cfc7e1183ea44
Ryan Joseph Lampe <rlampe@cse.unl.edu>
2018-09-06 15:11:48 -0500

    I changed numDigits to match

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -30,7 +30,7 @@ int isKaprekar(int n) {
 
   int i;
   long square = n * (long) n;
-  int numDigits = (int) log10(n) + 1;
+  int numberOfDigits = (int) log10(n) + 1;
   long modulus = 0;
   long first, second;
 
